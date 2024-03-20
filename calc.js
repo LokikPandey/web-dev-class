137ac8961cf84ae56fc5ebf035b8c6323627139d
Lokik <uditpandey546@gmail.com>
2024-03-20 15:27:29 +0530

    hi

---
+++ calc.js
@@ -0,0 +1,25 @@
+const display=document.getElementById("display")
+
+function addtodisplay(input){
+    display.value+=input
+}
+
+function clearscreen(){
+    display.value='';
+}
+function deletel(){
+    display.value=display.value.slice(0,-1)
+}
+function calc()
+{
+    display.value=eval(display.value)
+}
+function calc(){
+try{
+    display.value=eval(display.value);
+}
+catch(error)
+{
+    display.value='Error';
+}
+}
